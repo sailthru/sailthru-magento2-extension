7ec2cfc3bcfab3196b5a9d7e776ea6567b5897f8
Alex Silverman <alex@silverswatch.com>
2016-08-02 15:38:50 -0400

    Adding full attribute spectrum to product update vars

--- Plugin/ProductIntercept.php
+++ Plugin/ProductIntercept.php
@@ -12,6 +12,30 @@ use Sailthru\MageSail\Helper\Api;
 
 class ProductIntercept
 {
+    // need some things from EAV attributes, directly - these, we'd rather get from Product.
+    public static $unusedVarKeys = [
+        'row_id',
+        'type_id',
+        'attribute_set_id',
+        'media_gallery',
+        'thumbnail',
+        'shipment_type',
+        'url_key',
+        'price_view',
+        'msrp_display_actual_price_type',
+        'page_layout',
+        'options_container',
+        'custom_design',
+        'custom_layout',
+        'gift_message_available',
+        'category_ids',
+        'image',
+        'small_image',
+        'visibility',
+        'relatedProductIds',
+        'upSellProductIds',
+        'description',
+    ];
 
 	public function __construct(Api $sailthru, StoreManagerInterface $storeManager, ProductHelper $productHelper, ImageHelper $imageHelper, ConfigProduct $cpModel){
 		$this->sailthru = $sailthru;
@@ -43,11 +67,12 @@ class ProductIntercept
     {
         
         // scope fix for intercept launched from backoffice, which causes admin URLs for products
-        // $storeScopes = $product->getStoreIds();
-        // $storeId = $storeScopes ? $storeScopes[0] : $product->getStoreId();
-        // if ($storeId) $product->setStoreId($storeId);
-        // $this->_storeManager->setCurrentStore($storeId);
+        $storeScopes = $product->getStoreIds();
+        $storeId = $storeScopes ? $storeScopes[0] : $product->getStoreId();
+        if ($storeId) $product->setStoreId($storeId);
+        $this->_storeManager->setCurrentStore($storeId);
         $parents = $this->cpModel->getParentIdsByChild($product->getId());
+        $usable_attributes = $this->_getProductAttributeValues($product);
         try {
             $data = [
                 'url'   => $parents ? $this->getProductFragmentedUrl($product, $parents[0]) : $product->setStoreId($storeId)->getProductUrl(true),
@@ -56,42 +81,38 @@ class ProductIntercept
                 'spider' => 1,
                 'price' => $product->getPrice() * 100,
                 'description' => strip_tags($product->getDescription()),
-                'tags' => htmlspecialchars($product->getMetaKeyword()),
+                'tags' => $this->getTags($product, $usable_attributes),
                 'images' => array(),
                 'inventory' => $product->getStockData()["qty"],
-                // 'vars' => [
-                    // 'options2' => $product->getOptions(),
-                    // 'options3' => $product->getCustomOptions(),
-                    // 'codes' => $product->getCustomAttributesCodesf(),
-                    // 'sku' => $product->getSku(),
-                    // 'storeId' => $storeId,
-                    // 'typeId' => $product->getTypeId(),
-                    // 'status' => $product->getStatus(),
-                    // 'categoryId' => $product->getCategory(),
-                    // 'categoryIds' => $product->getCategoryIds(),
-                    // 'websiteIds' => $product->getWebsiteIds(),
-                    // 'storeIds'  => $product->getStoreIds(),
-                    // 'price' => $product->getPrice() * 100,
-                    // 'groupPrice' => $product->getGroupPrice(),
-                    // 'formatedPrice' => $product->getFormatedPrice(),
-                    // 'calculatedFinalPrice' => $product->getCalculatedFinalPrice(),
-                    // 'minimalPrice' => $product->getMinimalPrice(),
-                    // 'specialPrice' => $product->getSpecialPrice(),
-                    // 'specialFromDate' => $product->getSpecialFromDate(),
-                    // 'specialToDate'  => $product->getSpecialToDate(),
-                    // 'relatedProductIds' => $product->getRelatedProductIds(),
-                    // 'upSellProductIds' => $product->getUpSellProductIds(),
-                    // 'getCrossSellProductIds' => $product->getCrossSellProductIds(),
-                    // 'isConfigurable'  => $product->canConfigure(),
-                    // 'isSalable' => $product->isSalable(),
-                    // 'isAvailable'  => $product->isAvailable(),
-                    // 'isVirtual'  => $product->isVirtual(),
-                    // 'isInStock'  => $product->isInStock(),
-                    // 'weight'  => $product->getWeight(),
-                    // 'isVisible' => $this->productHelper->canShow($product)
-                    // 'product' => $product->toArray(),
-                // ],
-                'vars' => $this->_getProductAttributeValues($product),
+                'vars' => [
+                    'options2' => $this->cpModel->getSelectedAttributesInfo($product),
+                    'options3' => $product->getCustomOptions('attributes'),
+                    'sku' => $product->getSku(),
+                    'storeId' => $product->getStoreId(),
+                    'typeId' => $product->getTypeId(),
+                    'status' => $product->getStatus(),
+                    'categories' => $this->getCategories($product),
+                    'websiteIds' => $product->getWebsiteIds(),
+                    'storeIds'  => $product->getStoreIds(),
+                    'price' => $product->getPrice() * 100,
+                    'groupPrice' => $product->getGroupPrice(),
+                    'formatedPrice' => $product->getFormatedPrice(),
+                    'calculatedFinalPrice' => $product->getCalculatedFinalPrice(),
+                    'minimalPrice' => $product->getMinimalPrice(),
+                    'specialPrice' => $product->getSpecialPrice(),
+                    'specialFromDate' => $product->getSpecialFromDate(),
+                    'specialToDate'  => $product->getSpecialToDate(),
+                    'relatedProductIds' => $product->getRelatedProductIds(),
+                    'upSellProductIds' => $product->getUpSellProductIds(),
+                    'getCrossSellProductIds' => $product->getCrossSellProductIds(),
+                    'isConfigurable'  => $product->canConfigure(),
+                    'isSalable' => $product->isSalable(),
+                    'isAvailable'  => $product->isAvailable(),
+                    'isVirtual'  => $product->isVirtual(),
+                    'isInStock'  => $product->isInStock(),
+                    'weight'  => $product->getWeight(),
+                    'isVisible' => $this->productHelper->canShow($product)
+                ] + $usable_attributes,
             ];
             // Add product images
             if($image = $product->getImage()) {
@@ -134,7 +155,24 @@ class ProductIntercept
     }
 
     private function attributeGate($attr){
-        if ($attr == "thumbnail" or strpos($attr, "media") !== false or strpos($attr, "image") !== false) return false;
+        if (in_array($attr, self::$unusedVarKeys)) return false;
         return true;
     }
+
+    public function getTags($product, $attributes){
+        $meta = htmlspecialchars($product->getMetaKeywords());
+        return $meta;
+    }
+
+    public function getCategories($product){
+        $collection = $product->getCategoryCollection();
+        $items = $collection->addAttributeToSelect('name')->getItems();
+        $categories = [];
+        foreach ($items as $item) {
+            $this->sailthru->logger(get_object_vars($item));
+            $categories[] = $item->getName();
+        }
+        return $categories;
+    }
+
 }
