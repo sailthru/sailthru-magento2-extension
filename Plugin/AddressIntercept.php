88705d613c01eb2bad6f4b0049ad41c616e75de9
Alex Silverman <alex@silverswatch.com>
2016-08-16 11:45:51 -0400

    CodeSniffed + tested

--- Plugin/AddressIntercept.php
+++ Plugin/AddressIntercept.php
@@ -7,28 +7,29 @@ use Sailthru\MageSail\Helper\Api;
 class AddressIntercept
 {
 
-	public function __construct(Api $sailthru){
-		$this->sailthru = $sailthru;
-	}
+    public function __construct(Api $sailthru)
+    {
+        $this->sailthru = $sailthru;
+    }
 
-	public function afterSave($subject, $addressResult){
-		try {	
-			$customer = $addressResult->getCustomer();
-			$sid	  = $customer->getData('sailthru_id');
-			$email	  = $customer->getEmail();
-			$addressVars  = $this->sailthru->getAddressVars($addressResult);
-			$data = [
-				'id' => $sid ? $sid : $email,
-				'vars' => $addressVars,
-			];
+    public function afterSave($subject, $addressResult)
+    {
+        try {
+            $customer = $addressResult->getCustomer();
+            $sid      = $customer->getData('sailthru_id');
+            $email    = $customer->getEmail();
+            $addressVars  = $this->sailthru->getAddressVars($addressResult);
+            $data = [
+                'id' => $sid ? $sid : $email,
+                'vars' => $addressVars,
+            ];
             $this->sailthru->client->_eventType = 'CustomerAddressUpdate';
-		    $response = $this->sailthru->client->apiPost('user', $data);
-		    $this->sailthru->client->_eventType = '';
-		} catch (\Exception $e){
-			$this->sailthru->logger($e->getMessage());
-		} finally {
-			return $addressResult;
-		}
-	}
-
+            $response = $this->sailthru->client->apiPost('user', $data);
+            $this->sailthru->client->_eventType = '';
+        } catch (\Exception $e) {
+            $this->sailthru->logger($e->getMessage());
+        } finally {
+            return $addressResult;
+        }
+    }
 }
