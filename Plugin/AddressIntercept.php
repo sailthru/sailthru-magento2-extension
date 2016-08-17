20d5d88b85fe5036a0cf24687a33a79cadb51a76
Alex Silverman <alex@silverswatch.com>
2016-08-17 18:39:50 -0400

    Fixed billing-only filtering for AddressIntercept

--- Plugin/AddressIntercept.php
+++ Plugin/AddressIntercept.php
@@ -14,21 +14,26 @@ class AddressIntercept
 
     public function afterSave($subject, $addressResult)
     {
-        try {
-            $customer = $addressResult->getCustomer();
-            $sid      = $customer->getData('sailthru_id');
-            $email    = $customer->getEmail();
-            $addressVars  = $this->sailthru->getAddressVars($addressResult);
-            $data = [
-                'id' => $sid ? $sid : $email,
-                'vars' => $addressVars,
-            ];
-            $this->sailthru->client->_eventType = 'CustomerAddressUpdate';
-            $response = $this->sailthru->client->apiPost('user', $data);
-            $this->sailthru->client->_eventType = '';
-        } catch (\Exception $e) {
-            $this->sailthru->logger($e->getMessage());
-        } finally {
+        $billing = $addressResult->getDataModel()->isDefaultBilling();
+        if ($billing) {
+            try {
+                $customer = $addressResult->getCustomer();
+                $sid      = $customer->getData('sailthru_id');
+                $email    = $customer->getEmail();
+                $addressVars  = $this->sailthru->getAddressVars($addressResult);
+                $data = [
+                    'id' => $sid ? $sid : $email,
+                    'vars' => $addressVars,
+                ];
+                $this->sailthru->client->_eventType = 'CustomerAddressUpdate';
+                $response = $this->sailthru->client->apiPost('user', $data);
+                $this->sailthru->client->_eventType = '';
+            } catch (\Exception $e) {
+                $this->sailthru->logger($e->getMessage());
+            } finally {
+                return $addressResult;
+            }
+        } else {
             return $addressResult;
         }
     }
