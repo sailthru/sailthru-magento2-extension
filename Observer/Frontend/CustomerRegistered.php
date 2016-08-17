fd95c1c1b426aaf669b8adca26e301a8f3edc9ee
Alex Silverman <alex@silverswatch.com>
2016-08-17 17:33:53 -0400

    codesniffed register observer + tested

--- Observer/Frontend/CustomerRegistered.php
+++ Observer/Frontend/CustomerRegistered.php
@@ -10,48 +10,29 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Module\Manager;
 use Magento\Newsletter\Model\Subscriber;
 
-
 class CustomerRegistered implements ObserverInterface
 {
-
 
     const XML_ONREGISTER_LIST_ENABLED = "magesail_lists/lists/enable_signup_list";
     const XML_ONREGISTER_LIST_VALUE = "magesail_lists/lists/signup_list";
     const XML_NEWSLETTER_LIST_ENABLED = "magesail_lists/lists/enable_newsletter";
     const XML_NEWSLETTER_LIST_VALUE = "magesail_lists/lists/newsletter_list";
 
-    /**
-     * @var Session
-     */
-    protected $customerSession;
-
-    /**
-     * @var Api
-     */
-    protected $sailthru;
-
-    /**
-     * Module manager
-     *
-     * @var Manager
-     */
-    private $moduleManager;
-
-
-    public function __construct(Api $sailthru, Manager $moduleManager, Subscriber $subscribeCtrl) {
+    public function __construct(
+        Api $sailthru,
+        Manager $moduleManager,
+        Subscriber $subscribeCtrl
+    ) {
         $this->moduleManager = $moduleManager;
         $this->sailthru = $sailthru;
         $this->subscribeCtrl = $subscribeCtrl;
     }
 
-
     public function execute(Observer $observer)
     {
         if ($this->moduleManager->isEnabled('Sailthru_MageSail')) {
-
             $customer = $observer->getData('customer');
             $email = $customer->getEmail();
-
             try {
                 $this->sailthru->client->_eventType = 'CustomerRegister';
                 $data = [
@@ -67,15 +48,14 @@ class CustomerRegistered implements ObserverInterface
                         ]
                 ];
 
-                if ($this->sailthru->getSettingsVal(self::XML_ONREGISTER_LIST_ENABLED)){
+                if ($this->sailthru->getSettingsVal(self::XML_ONREGISTER_LIST_ENABLED)) {
                     $list = $this->sailthru->getSettingsVal(self::XML_ONREGISTER_LIST_VALUE);
                     $data["lists"] = [ $list => 1 ];
                 }
+                
                 $response = $this->sailthru->client->apiPost('user', $data);
                 $this->sailthru->hid->set($response["keys"]["cookie"]);
-            } catch(\Sailthru_Email_Model_Client_Exception $e) {
-                $this->sailthru->logger($e);
-            } catch(\Exception $e) {
+            } catch (\Exception $e) {
                 $this->sailthru->logger($e);
             }
         }
