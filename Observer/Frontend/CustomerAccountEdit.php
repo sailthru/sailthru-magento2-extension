f77423950981e1cf3c897ad5e53ef6c940431978
Alex Silverman <alex@silverswatch.com>
2016-08-17 17:21:08 -0400

    Codesniffed + Tested

--- Observer/Frontend/CustomerAccountEdit.php
+++ Observer/Frontend/CustomerAccountEdit.php
@@ -11,45 +11,28 @@ use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Module\Manager;
 use Magento\Store\Model\StoreManagerInterface;
 
-
 class CustomerAccountEdit implements ObserverInterface
 {
-
 
     const SETTING_ENABLED = "magesail_lists/lists/enable_signup_list";
 
     const SETTING_VALUE = "magesail_lists/lists/signup_list";
 
-    /**
-     * @var Session
-     */
-    protected $customerModel;
-
-    /**
-     * @var Api
-     */
-    protected $sailthru;
-
-    /**
-     * Module manager
-     *
-     * @var Manager
-     */
-    private $moduleManager;
-
-
-    public function __construct(Api $sailthru, Manager $moduleManager, Customer $customerModel, StoreManagerInterface $storeManager) {
+    public function __construct(
+        Api $sailthru,
+        Manager $moduleManager,
+        Customer $customerModel,
+        StoreManagerInterface $storeManager
+    ) {
         $this->moduleManager = $moduleManager;
         $this->sailthru = $sailthru;
         $this->customerModel = $customerModel;
         $this->storeManager = $storeManager;
     }
 
-
     public function execute(Observer $observer)
     {
         if ($this->moduleManager->isEnabled('Sailthru_MageSail')) {
-
             $websiteId  = $this->storeManager->getWebsite()->getWebsiteId();
             $this->customerModel->setWebsiteId($websiteId);
             $email = $observer->getData('email');
@@ -61,7 +44,7 @@ class CustomerAccountEdit implements ObserverInterface
 
                 $data = [
                         'id' => $sid ? $sid : $email,
-                        'fields' => ['keys' => 1], 
+                        'fields' => ['keys' => 1],
                         'keysconflict' => 'merge',
                         'keys'=> [
                             'email' => $email
@@ -73,21 +56,21 @@ class CustomerAccountEdit implements ObserverInterface
                         ]
                 ];
 
-                // if($address = $this->sailthru->getAddressVarsByCustomer($customer)) $data['vars'] += $address;
+                if ($address = $this->sailthru->getAddressVarsByCustomer($customer)) {
+                    $data['vars'] += $address;
+                }
 
-                if($customer->getCustomAttribute('is_subscribed')){
+                if ($customer->getCustomAttribute('is_subscribed')) {
                     $data["lists"] = ["Newsletter"=>1];
                 }
 
                 $response = $this->sailthru->client->apiPost('user', $data);
                 $this->sailthru->hid->set($response["keys"]["cookie"]);
-
-            } catch(\Sailthru_Email_Model_Client_Exception $e) {
+            } catch (\Sailthru_Email_Model_Client_Exception $e) {
                 $this->sailthru->logger($e);
-            } catch(\Exception $e) {
+            } catch (\Exception $e) {
                 $this->sailthru->logger($e);
             }
         }
     }
-
 }
