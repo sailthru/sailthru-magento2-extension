5fdd1497c5ae24fd9259682ff139f7f463ff150a
Alex Silverman <alex@silverswatch.com>
2016-08-17 18:39:20 -0400

    Codesniffed Login Observer + Tested

--- Observer/Frontend/CustomerLoggedIn.php
+++ Observer/Frontend/CustomerLoggedIn.php
@@ -9,68 +9,39 @@ use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Module\Manager;
 
-
 class CustomerLoggedIn implements ObserverInterface
 {
-    /**
-     * @var Session
-     */
-    protected $customerSession;
 
-    /**
-     * @var Api
-     */
-    protected $sailthru;
-
-    /**
-     * Module manager
-     *
-     * @var Manager
-     */
-    private $moduleManager;
-
-
-    public function __construct(Api $sailthru, Manager $moduleManager) {
-        // $this->customerSession = $customerSession;
-        $this->moduleManager = $moduleManager;
+    public function __construct(Api $sailthru)
+    {
         $this->sailthru = $sailthru;
-        // $this->_cookieManager = $cookieManager;
-        // $this->_cookieMetadataFactory = $cookieMetadataFactory;
     }
-
 
     public function execute(Observer $observer)
     {
-        if ($this->moduleManager->isEnabled('Sailthru_MageSail')) {
+        $customer = $observer->getData('customer');
+        $sid = $customer->getData('sailthru_id');
 
-            $customer = $observer->getData('customer');
-            $sid = $customer->getData('sailthru_id');
-
-            try {
-                $this->sailthru->client->_eventType = 'CustomerLogin';
-                $data = [
-                        'id' => $sid ? $sid : $customer->getEmail(),
-                        'fields' => [
-                            'keys' => 1,
-                            'engagement' => 1,
-                            'activity' => 1,
-                            'email' => $customer->getEmail(),
-                            // 'extid' => $customer->getEntityId()
-                        ]
-                ];
-                $response = $this->sailthru->client->apiPost('user', $data);
-                if (!$sid){
-                    $customerData = $customer->getDataModel();
-                    $customerData->setCustomAttribute('sailthru_id', $response['keys']['sid']);
-                    $customer->updateData($customerData);
-                    $customer->save();
-                }
-                $this->sailthru->hid->set($response["keys"]["cookie"]);
-            } catch(\Sailthru_Email_Model_Client_Exception $e) {
-                $this->sailthru->logger($e);
-            } catch(\Exception $e) {
-                $this->sailthru->logger($e);
+        try {
+            $this->sailthru->client->_eventType = 'CustomerLogin';
+            $data = [
+                    'id' => $sid ? $sid : $customer->getEmail(),
+                    'fields' => [
+                        'keys' => 1,
+                        'engagement' => 1,
+                        'activity' => 1,
+                    ]
+            ];
+            $response = $this->sailthru->client->apiPost('user', $data);
+            if (!$sid) {
+                $customerData = $customer->getDataModel();
+                $customerData->setCustomAttribute('sailthru_id', $response['keys']['sid']);
+                $customer->updateData($customerData);
+                $customer->save();
             }
+            $this->sailthru->hid->set($response["keys"]["cookie"]);
+        } catch (\Exception $e) {
+            $this->sailthru->logger($e);
         }
     }
 }
