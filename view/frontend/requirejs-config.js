56a2c8aeb346d89dbcd50fe5493b4bf478ca29a7
Alex Silverman <asilverman@sailthru.com>
2017-02-10 18:11:57 -0500

    updating SPM to latest version

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,17 +1,6 @@
-// var config = {
-//     paths: {
-//     "Sailthru": "https://ak.sail-horizon.com/onsite/personalize.v0.0.4.min",
-//     },
-//     waitSeconds: 300,
-//     shim: {
-//         Sailthru: {
-//             exports: 'Sailthru'
-//         },
-//     }
-// };
 var config = {
     paths: {
-    	"Sailthru": "http://127.0.0.1:8080/spm.v1.min",
+    	"Sailthru": "https://ak.sail-horizon.com/spm/spm.v1.min",
     },
     waitSeconds: 300,
     shim: {
--- view/frontend/web/spm.js
+++ view/frontend/web/spm.js
@@ -1,10 +1,5 @@
-// require(["Sailthru"], function (Sailthru) {
-//     console.log(Sailthru);
-//     console.log('Want to use SPM with JSON? Configure it here!');
-// }); 
 require(["Sailthru"], function (Sailthru) {
-    console.log(Sailthru);
-    console.log('Want to use SPM with JSON?! WOO');
-    var client_id = '45124590f1851dcdcf7ac2e38d793cd2';
+	console.log("Edit this file to use SPM onsite!");
+    var client_id = '<ENTER_CLIENT_ID_HERE>';
     Sailthru.init({ customerId: client_id});
 });
