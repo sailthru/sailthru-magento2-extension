2533313556089e1ebd36a3a28efef5bc3a1ace33
Alex Silverman <asilverman@sailthru.com>
2017-07-06 16:27:30 -0400

    refactoring

--- Model/Config/Source/Verifiedemails.php
+++ Model/Config/Source/Verifiedemails.php
@@ -2,27 +2,11 @@
  
 namespace Sailthru\MageSail\Model\Config\Source;
 
-use \Magento\Framework\Option\ArrayInterface;
-use \Sailthru\MageSail\Helper\ClientManager;
-use \Sailthru\MageSail\Helper\Settings as SailthruSettings;
-
-class Verifiedemails implements ArrayInterface
+class VerifiedEmails extends AbstractSource
 {
 
-    private $clientManager;
-
-    public function __construct(ClientManager $clientManager)
+    protected function getDisplayData()
     {
-        $this->clientManager = $clientManager;
-    }
-
-    public function toOptionArray()
-    {
-        if (!$this->clientManager->isValid()) {
-            return [
-                ['value'=>0, 'label'=>__('Please Enter Valid Credentials')]
-                ];
-        }
         $emails = $this->clientManager->getClient()->getVerifiedSenders();
         $sender_options = [
             ['value'=> 0, 'label'=>' ']
