3deac0ab2226dd89de51bda5b3effadb2fb06646
Alex Silverman <asilverman@sailthru.com>
2018-06-28 11:31:14 -0400

    refactoring to use new scope tools

--- Observer/Frontend/CustomerLoggedIn.php
+++ Observer/Frontend/CustomerLoggedIn.php
@@ -2,42 +2,48 @@
 
 namespace Sailthru\MageSail\Observer\Frontend;
 
+use Magento\Customer\Model\Customer;
 use Sailthru\MageSail\Helper\ClientManager;
 use Sailthru\MageSail\Cookie\Hid as SailthruCookie;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Sailthru\MageSail\Logger;
 
 class CustomerLoggedIn implements ObserverInterface
 {
 
     private $sailthruCookie;
-    private $sailthruClient;
+    private $clientManager;
+    private $logger;
 
     public function __construct(
         ClientManager $clientManager,
-        SailthruCookie $sailthruCookie
+        SailthruCookie $sailthruCookie,
+        Logger $logger
     ) {
         $this->sailthruCookie = $sailthruCookie;
-        $this->sailthruClient = $clientManager;
+        $this->clientManager = $clientManager;
+        $this->logger = $logger;
     }
 
     public function execute(Observer $observer)
     {
+        /** @var Customer $customer */
         $customer = $observer->getData('customer');
-        $this->sailthruClient = $this->sailthruClient->getClient(true, $customer->getStore()->getId());
+        $this->logger->info("customer", $customer->debug());
+        $client = $this->clientManager->getClient(true, $customer->getStore()->getId());
         $sid = $customer->getData('sailthru_id');
-
+        $client->_eventType = 'CustomerLogin';
+        $data = [
+            'id' => $sid ? $sid : $customer->getEmail(),
+            'fields' => [
+                'keys' => 1,
+                'engagement' => 1,
+                'activity' => 1,
+            ]
+        ];
         try {
-            $this->sailthruClient->_eventType = 'CustomerLogin';
-            $data = [
-                    'id' => $sid ? $sid : $customer->getEmail(),
-                    'fields' => [
-                        'keys' => 1,
-                        'engagement' => 1,
-                        'activity' => 1,
-                    ]
-            ];
-            $response = $this->sailthruClient->apiPost('user', $data);
+            $response = $client->apiPost('user', $data);
             if (!$sid) {
                 $customerData = $customer->getDataModel();
                 $customerData->setCustomAttribute('sailthru_id', $response['keys']['sid']);
@@ -46,7 +52,7 @@ class CustomerLoggedIn implements ObserverInterface
             }
             $this->sailthruCookie->set($response["keys"]["cookie"]);
         } catch (\Exception $e) {
-            $this->sailthruClient->logger($e);
+            $this->logger->err("Exception logging in user with Sailthru: {$e->getMessage()}", ["exception" => $e]);
         }
     }
 }
