a9157e6fb1ab738bae14eb23b4affed577fe3a8e
ravikiran2790 <ravikiran.patil@sailthru.com>
2024-12-06 18:24:09 +0530

    [INT-1170] INT-1170 Added multiple ways to get the isSubscribe value

--- Observer/Frontend/CustomerRegistered.php
+++ Observer/Frontend/CustomerRegistered.php
@@ -4,11 +4,17 @@ namespace Sailthru\MageSail\Observer\Frontend;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Mview\View\SubscriptionFactory;
 use Sailthru\MageSail\Helper\ClientManager;
 use Sailthru\MageSail\Helper\Settings as SailthruSettings;
 use Sailthru\MageSail\Cookie\Hid as SailthruCookie;
 use Sailthru\MageSail\Helper\VarHelper;
 use Sailthru\MageSail\Logger;
+use Magento\Customer\Model\CustomerRegistry;
+use Magento\Customer\Model\CustomerFactory;
+use Magento\CUstomer\Model\Customer;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Newsletter\Model\Subscriber;
 
 class CustomerRegistered implements ObserverInterface
 {
@@ -16,32 +22,76 @@ class CustomerRegistered implements ObserverInterface
     private $sailthruSettings;
     private $sailthruCookie;
     private $sailthruVars;
-    /** @var Logger */
     private $logger;
+    private $customerRegistry;
+    protected $customerFactory;
+    private $customerModel;
+    private $storeManager;
+    private $subscriber;
 
     public function __construct(
         ClientManager $clientManager,
         SailthruSettings $sailthruSettings,
         SailthruCookie $sailthruCookie,
         VarHelper $sailthruVars,
-        Logger $logger
+        Logger $logger,
+        CustomerRegistry $customerRegistry,
+        CustomerFactory $customerFactory,
+        Customer $customerModel,
+        StoreManagerInterface $storeManager,
+        Subscriber $subscriber
     ) {
         $this->clientManager = $clientManager;
         $this->sailthruSettings = $sailthruSettings;
         $this->sailthruCookie = $sailthruCookie;
         $this->sailthruVars = $sailthruVars;
         $this->logger = $logger;
+        $this->customerRegistry = $customerRegistry;
+        $this->customerFactory = $customerFactory;
+        $this->customerModel = $customerModel;
+        $this->storeManager = $storeManager;
+        $this->subscriber = $subscriber;
     }
 
     public function execute(Observer $observer)
     {
         $customer = $observer->getData('customer');
         try {
+            // Get is_subscribed value by using Customer Model
+            $websiteId = $this->storeManager->getWebsite()->getWebsiteId();
+            $this->customerModel->setWebsiteId($websiteId);
+            $customerDataByEvent = $observer->getEvent()->getCustomer();
+            $customerEmail = $observer->getData('email');
+            $customerModel = $this->customerModel->loadByEmail($customerEmail);
+            $loadByCustomerModel = $customerModel->getCustomAttribute('is_subscribed');
+
+            // Get is_subscribed by Subscriber class (Getting is_subscribed value using this way on local)
+            $subscriberDetails = $this->subscriber->loadByEmail($customer->getEmail());
+            $status = $subscriberDetails->getStatus();
+            $isSubscribedNewValue = ($status == Subscriber::STATUS_SUBSCRIBED ? 1 : 0);
+            $this->logger->info("#############SubscribedNewValue [$isSubscribedNewValue]###############");
+
+            // Get is_subscribed value by using customerFactory
+            $loadCustomerByCustomerFactory = $this->customerFactory->create()->load($customerDataByEvent->getId());
+            $isSubscribedValueFromCustomerFactory = $loadCustomerByCustomerFactory->getIsSubscribed();
+            $isSubscribedValueFromDataModel = $loadCustomerByCustomerFactory->getDataModel()->getCustomAttribute('is_subscribed');
+
+            // Get is_subscribed value by using customerRegistry
+            $loadCustomerByCustomerRegistry = $this->customerRegistry->retrieve($customerDataByEvent->getId());
+            $isSubscribedValueFromCustomerRegistry = $loadCustomerByCustomerRegistry->getIsSubscribed();
+
+            //  Get is_subscribed value by using Event AccountController  (Getting is_subscribed value using this way on local)
             $isSubscribed = $observer->getEvent()->getAccountController()->getRequest()->getParam('is_subscribed');
-            $optOutEmail = $isSubscribed == '1' ? "none" : "basic";
-            $this->logger->info("CustomerRegistration IsSubscriptionValues Observer: [$observer] - [$observer->getValue()] & Customer: [$customer] - [$customer->getValue()] & isSubscribed: [$customer->getCustomAttribute('is_subscribed')] & [$isSubscribed] & [$optOutEmail]");
+
+            if ($isSubscribed != '') { // Troubleshooting: If $isSubscribed is empty then get value from $isSubscribedNewValue
+                $optOutEmail = $isSubscribed == '1' ? "none" : "basic";
+            } else {
+                $optOutEmail = $isSubscribedNewValue == '1' ? "none" : "basic";
+            }
+            $this->logger->info("CustomerRegistration IsSubscriptionValues loadCustomerByCustomerFactory: [$isSubscribedValueFromCustomerFactory] loadCustomerByCustomerRegistry: [$isSubscribedValueFromCustomerRegistry] & isSubscribedValueFromDataModel: [$isSubscribedValueFromDataModel]");
+            $this->logger->info("CustomerRegistration IsSubscriptionValues loadByCustomerModel: [$loadByCustomerModel] & AccountController: [$isSubscribed] & [$optOutEmail] & isSubscribedNewValue [$isSubscribedNewValue]");
         } catch (\Exception $e) {
-            $this->logger->error("Error while getting is_subscribed # {$e->getMessage()}");
+            $this->logger->error("Error in CustomerRegistration # {$e->getMessage()}");
             $optOutEmail = "basic";
         }
         $storeId = $customer->getStoreId();
